085c7e26969b0506955151a6e013c4cba738f11b
smokd <wtsithole@icloud.com>
2025-07-26 07:45:14 +0200

    adjusting layout.tsx, nexjs using approute,

--- admill-landing-page/next.config.ts
+++ admill-landing-page/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  output: 'export', // CRITICAL for static site generation
+  images: {
+    unoptimized: true, // Recommended for static export if using Next.js Image component
+}};
 
 export default nextConfig;
