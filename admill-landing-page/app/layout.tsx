36899254627c71b45affb1859877ee6d1ccc2e45
smokd <smokd@users.noreply.github.com>
2026-08-17 21:18:35 +0200

    fixed matomo script,now works

--- admill-landing-page/app/layout.tsx
+++ admill-landing-page/app/layout.tsx
@@ -225,17 +225,23 @@ export default function RootLayout({
         `}
       </Script>
 
-      {/* Matomo Tag Manager */}
-      <Script>
+      {/*<!-- Matomo -->*/}
+      <script>
         {`
-          var _mtm = window._mtm = window._mtm || [];
-          _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
-          (function() {
-            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-            g.async=true; g.src='https://admill.co.zw/matomo/js/container_Fg7DkXUJ.js'; s.parentNode.insertBefore(g,s);
-          })();
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="//admill.co.zw/matomo/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '1']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+
         `}
-      </Script>
+      </script>
 
       {/* Tawk.to Live Chat Widget */}
       <Script id="tawk-to-widget" strategy="lazyOnload">
