21bdf2c058883a43b924e6de9587d46c70111385
smokd <smokd@users.noreply.github.com>
2026-09-09 20:29:53 +0200

    Fix broken JSX in matomo block (removed stray nested Script tag)

--- admill-landing-page/app/layout.tsx
+++ admill-landing-page/app/layout.tsx
@@ -179,7 +179,6 @@ export default function RootLayout({
 
       {/*<!-- Matomo -->*/}
       <script>
-      <Script id="matomo-tag-manager" strategy="afterInteractive">
         {`
           var _mtm = window._mtm = window._mtm || [];
           _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
